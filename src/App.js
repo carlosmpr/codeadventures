e12585f1d51245236b160ce5ae28fb7e52166a96
Carlos Polanco <carlos.polanco0508@gmail.com>
2022-09-27 13:37:54 -0400

    Change page design

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
+    <div >
       <Navbar />
-      <div className="w-full h-screen flex flex-col items-center ">
-        <h1 className="z-20 text-7xl font-thin text-gray-500 mt-20 " >Ready for your next Adventure?</h1>
-        <h1 className="z-20 text-6xl  font-bold mt-2">Code Adventures </h1>
+      <div className="w-full h-screen flex flex-col items-center text-white ">
+        <h1 className="z-20 text-7xl  font-bold mt-20 " >Ready for your next Adventure?</h1>
+        <h1 className="z-20 text-6xl  text-yellow-100  mt-2">Code Adventures </h1>
         <p className="z-20   mt-4 mb-4">Currently under development </p>
        <Button />
         <div className="w-full h-full absolute">
-        <iframe src='https://my.spline.design/altarskycopy-851ffaede4f76a2f1b7b784869f37e60/' frameborder='0' width='100%' height='100%' title="island"></iframe>
+        <iframe src='https://my.spline.design/altarskycopy-851ffaede4f76a2f1b7b784869f37e60/' frameborder='0' width='100%' height='100%' title="scene"></iframe>
         </div>
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import Button from './Button'
 
 export default function Navbar() {
   return (
-    <div className='w-full pl-12 pr-12 pt-4 pb-4 border-b h-18 flex items-center justify-between rounded-br-lg'>
-        <p className='font-bold text-2xl'>CAD</p>
-        <div className='flex gap-x-16 text-gray-500'>
+    <div className='w-full  text-white pl-12 pr-12 pt-4 pb-4 h-18 flex items-center justify-between rounded-br-lg  '>
+        <p className='font-bold text-2xl text-yellow-100 '>CAD</p>
+        <div className='flex gap-x-16 text-gray-50'>
         <p>Home</p>
         <p>About</p>
         <p>Features</p>
